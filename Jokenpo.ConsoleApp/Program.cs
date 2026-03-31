41785dc8c72fa07bc63803974490beba3adb0b7a
cyb3rn3tic <steffenosmar@hotmail.com>
2026-03-31 12:54:33 -0300

    v1 do jogo

--- Jokenpo.ConsoleApp/Program.cs
+++ Jokenpo.ConsoleApp/Program.cs
@@ -1,9 +1,103 @@
-﻿namespace Jokenpo.ConsoleApp;
+﻿using System.Collections;
+using System.Security.Cryptography;
+
+namespace Jokenpo.ConsoleApp;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        do 
+        {
+            Console.WriteLine("------------------------");
+            Console.WriteLine("Bem-vindo ao Jokenpô!");
+            Console.WriteLine("------------------------");
+
+            Console.WriteLine("Escolha uma opção:");
+            Console.WriteLine("1 - Pedra");
+            Console.WriteLine("2 - Papel");
+            Console.WriteLine("3 - Tesoura");
+            Console.WriteLine("4 - Sair");
+
+            string opcao = Console.ReadLine()?.Trim() ?? ""; //// O "??" evita o aviso de valor nulo e o Trim() remove espaços acidentais
+
+            Random random = new Random();
+            int opcaoComputador = random.Next(1, 3);
+
+            switch (opcao)
+            {
+                case "1":
+                    Console.WriteLine("------------------------");
+                    Console.WriteLine("Você escolheu Pedra.");
+                    Console.WriteLine($"O computador escolheu {opcaoComputador}.");
+
+                    if (opcaoComputador == 1)
+                    {
+                        Console.WriteLine("\nEmpate!");
+                        Console.WriteLine("------------------------");
+                    }
+                    else if (opcaoComputador == 2)
+                    {
+                        Console.WriteLine("\nO computador venceu! Papel cobre Pedra.");
+                        Console.WriteLine("---------------------------------------");
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nParabéns! Você venceu! Pedra quebra Tesoura.");
+                        Console.WriteLine("--------------------------------------------");
+
+                    }
+                    continue;
+                case "2":
+                    Console.WriteLine("------------------------");
+                    Console.WriteLine("Você escolheu Papel.");
+                    Console.WriteLine($"O computador escolheu {opcaoComputador}.");
+
+                    if (opcaoComputador == 2)
+                    {
+                        Console.WriteLine("\nEmpate!");
+                        Console.WriteLine("------------------------");
+                    }
+                    else if (opcaoComputador == 1)
+                    {
+                        Console.WriteLine("\nParabéns! Você venceu! Papel cobre Pedra.");
+                        Console.WriteLine("-----------------------------------------");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nO computador venceu! Tesoura corta Papel.");
+                        Console.WriteLine("-----------------------------------------");
+                    }
+                    continue;
+                case "3":
+                    Console.WriteLine("------------------------");
+                    Console.WriteLine("Você escolheu Tesoura.");
+                    Console.WriteLine($"O computador escolheu {opcaoComputador}.");
+                    if (opcaoComputador == 3)
+                    {
+                        Console.WriteLine("\nEmpate!");
+                        Console.WriteLine("------------------------");
+                    }
+                    else if (opcaoComputador == 2)
+                    {
+                        Console.WriteLine("\nParabéns! Você venceu! Tesoura corta Papel.");
+                        Console.WriteLine("---------------------------------------------");
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nO computador venceu! Pedra quebra Tesoura.");
+                        Console.WriteLine("--------------------------------------------");
+                    }
+                    continue;
+                case "4":
+                    Console.WriteLine("Saindo do jogo. Até a próxima!");
+                    return;
+                default:
+                    Console.WriteLine("Opção inválida. Por favor, escolha uma opção válida.");
+                    continue;
+            }
+            
+        }while (true);
     }
 }
--- Jokenpo.ConsoleApp/obj/Debug/net10.0/Jokenpo.ConsoleApp.AssemblyInfo.cs
+++ Jokenpo.ConsoleApp/obj/Debug/net10.0/Jokenpo.ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Jokenpo.ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4c2e30e75d1d1c211ad27736b009dc3b7739950")]
 [assembly: System.Reflection.AssemblyProductAttribute("Jokenpo.ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Jokenpo.ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
